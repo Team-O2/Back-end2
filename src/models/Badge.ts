1c24b67510e9d9441d8e260c5b5c7d4928a2c19e
hyundang <hyunjin5697@yonsei.ac.kr>
2021-08-05 20:59:12 +0900

    feat: Badge model

---
+++ src/models/Badge.ts
@@ -0,0 +1,74 @@
+import {
+  Model,
+  Column,
+  Table,
+  PrimaryKey,
+  AutoIncrement,
+  Unique,
+  Default,
+} from "sequelize-typescript";
+
+@Table({ tableName: "badge", freezeTableName: true, underscored: true })
+export default class Badge extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Unique
+  @Column
+  id: number;
+
+  @Default(false)
+  @Column
+  welcomeBadge: Boolean;
+
+  @Default(false)
+  @Column
+  firstJoinBadge: Boolean;
+
+  @Default(false)
+  @Column
+  firstWriteBadge: Boolean;
+
+  @Default(false)
+  @Column
+  oneCommentBadge: Boolean;
+
+  @Default(false)
+  @Column
+  fiveCommentBadge: Boolean;
+
+  @Default(false)
+  @Column
+  oneLikeBadge: Boolean;
+
+  @Default(false)
+  @Column
+  fiveLikeBadge: Boolean;
+
+  @Default(false)
+  @Column
+  loginBadge: Boolean;
+
+  @Default(false)
+  @Column
+  marketingBadge: Boolean;
+
+  @Default(false)
+  @Column
+  firstReplyBadge: Boolean;
+
+  @Default(false)
+  @Column
+  learnMySelfScrapBadge: Boolean;
+
+  @Default(false)
+  @Column
+  challengeScrapBadge: Boolean;
+
+  @Default(false)
+  @Column
+  concertScrapBadge: Boolean;
+
+  @Default(0)
+  @Column
+  challengeBadge: number;
+}
