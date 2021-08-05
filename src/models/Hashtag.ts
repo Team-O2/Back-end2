4d5b0a68579f6e82001af66c6cf736124244fab6
hyundang <hyunjin5697@yonsei.ac.kr>
2021-08-05 21:09:47 +0900

    feat: Hashtag model

---
+++ src/models/Hashtag.ts
@@ -0,0 +1,26 @@
+import {
+  Model,
+  Column,
+  Table,
+  PrimaryKey,
+  AutoIncrement,
+  Unique,
+  ForeignKey,
+} from "sequelize-typescript";
+import Post from "./Post";
+
+@Table({ tableName: "hashtag", freezeTableName: true, underscored: true })
+export default class Hashtag extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Unique
+  @Column
+  id: number;
+
+  @ForeignKey(() => Post)
+  @Column
+  postID: number;
+
+  @Column
+  hashtag: string;
+}
