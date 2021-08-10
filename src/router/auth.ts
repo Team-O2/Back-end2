cc9142d31e25d69fa62c764ecad9d05420a091b4
holmir97 <holmir97@gmail.com>
2021-08-11 03:13:52 +0900

    feat: signup, signin routes

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { signupController } from "../controller/auth";
+import { signupController, signinController } from "../controller/auth";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
+router.post("/signin", signinController);
 
 export default router;
