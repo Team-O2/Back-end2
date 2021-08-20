80f8076d9e84dd75f6cc33d074d3626a720a6ccf
holmir97 <holmir97@gmail.com>
2021-08-21 00:20:15 +0900

    feat: concert index

--- src/DTO/index.ts
+++ src/DTO/index.ts
@@ -1,4 +1,5 @@
 export { default as adminDTO } from "./adminDTO";
 export { default as authDTO } from "./authDTO";
 export { default as challengeDTO } from "./challengeDTO";
+export { default as concertDTO } from "./concertDTO";
 export { default as commentDTO } from "./commentDTO";
