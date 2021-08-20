a02c2b4f8614aa72b10180b033128976dfbf91b6
holmir97 <holmir97@gmail.com>
2021-08-21 00:07:19 +0900

    feat: delete hastag

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./auth";
 import challengeRouter from "./challenge";
+import concertRouter from "./concert";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/challenge", challengeRouter);
+router.use("/concert", concertRouter);
 
 export default router;
