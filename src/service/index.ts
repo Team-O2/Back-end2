a63e5cb441ad92e7bdef6d33f807c9541c918253
holmir97 <holmir97@gmail.com>
2021-08-24 17:51:33 +0900

    feat: admin index

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,3 +2,4 @@ export { default as authService } from "./authService";
 export { default as userService } from "./userService";
 export { default as challengeService } from "./challengeService";
 export { default as concertService } from "./concertService";
+export { default as adminService } from "./adminService";
