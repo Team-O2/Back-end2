cde383a18b805e9e65e1fd9053fea7e8e82002a8
holmir97 <holmir97@gmail.com>
2021-08-30 16:07:20 +0900

    feat: email validator

---
+++ src/library/validator.ts
@@ -0,0 +1,13 @@
+function emailValidator(str) {
+  var email = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+  if (!email.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const validator = { emailValidator };
+
+export default validator;
