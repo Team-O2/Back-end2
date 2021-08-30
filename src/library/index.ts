074f6245d20df09257dc5522c61487f26a0163e1
holmir97 <holmir97@gmail.com>
2021-08-30 16:07:36 +0900

    feat: email validator index

--- src/library/index.ts
+++ src/library/index.ts
@@ -4,3 +4,4 @@ export { default as emailSender } from "./emailSender";
 export { default as jwt } from "./jwt";
 export { default as response } from "./response";
 export { default as returnCode } from "./returnCode";
+export { default as validator } from "./validator";
