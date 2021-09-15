99bae0c72cb9fca158f6149bc0e7d16739244e2e
hyundang <hyunjin5697@yonsei.ac.kr>
2021-09-15 15:04:52 +0900

    feat: default routing 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,22 @@
 import { Route53RecoveryCluster } from "aws-sdk";
-import express from "express";
+import express, { Request, Response } from "express";
 import authRouter from "./auth";
 import userRouter from "./user";
 import challengeRouter from "./challenge";
 import concertRouter from "./concert";
 import noticeRouter from "./notice";
 import adminRouter from "./admin";
+import { response, returnCode } from "../library";
 
 const router = express.Router();
 
+router.get("", async (req: Request, res: Response) => {
+  try {
+    response.basicResponse(res, returnCode.OK, "o2 api");
+  } catch (err) {
+    response.basicResponse(res, returnCode.INTERNAL_SERVER_ERROR, "서버 오류");
+  }
+});
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/challenge", challengeRouter);
